ebd7853a1893b5292d14c746ec30f66b0960af6f
Raimon <raimon.txollo@gmail.com>
2025-02-13 11:25:20 +0100

    Mario's video review

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,24 @@ import askUser from "./askUser.js";
 
 const songsTitles = ["40'", "Outsiders", "Lucid Dreams"];
 
-const songTitlePosition = Number(
+const songsTitlesPosition = Number(
   askUser("De la llista, quin número de cançó vols? ")
 );
 
-if (Number.isNaN(songTitlePosition)) {
-  // programación defensiva: primero los errores
+if (Number.isNaN(songsTitlesPosition)) {
   console.log("Siusplau, introdueix un número");
   process.exit();
 }
 
-const isValidPosition =
-  songTitlePosition >= 1 && songTitlePosition <= songsTitles.length;
-
-if (!isValidPosition) {
+if (songsTitlesPosition < 1 || songsTitlesPosition > songsTitles.length) {
   console.log(
-    `Error: en la teva llista hi ha ${songsTitles.length} cançons, no pots seleccionar la posició ${songTitlePosition}`
+    `Error: en la teva llista hi ha ${songsTitles.length} cançons, no pots seleccionar la posició ${songsTitlesPosition}`
   );
   process.exit();
 }
 
-const songTitleIndex = songTitlePosition - 1;
+const songTitleIndex = songsTitlesPosition - 1;
 
 const songTitle = songsTitles[songTitleIndex];
 
-console.log(songTitle);
+console.log(`La teva cançó: ${songTitle}`);
